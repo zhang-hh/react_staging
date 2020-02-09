49931bf23fa2f15117bf1e81e73abe5d69aa2bee
zhang-hh <94918628@qq.com>
2020-02-10 00:18:42 +0800

    完成编程式导航

--- src/pages/home-message/message.jsx
+++ src/pages/home-message/message.jsx
@@ -18,7 +18,19 @@ class Message extends Component {
 			})
 		},1000)
 	}
-
+	pushShow = (id) =>{
+		//this.props.history专门用来展示浏览器的历史记录
+		this.props.history.push(`/home/message/detail/${id}`)
+	}
+	replaceShow = (id) =>{
+		this.props.history.replace(`/home/message/detail/${id}`)
+	}
+	forword = () =>{
+		this.props.history.goForward();
+	}
+	back = () => {
+		this.props.history.goBack();
+	}
 	render() {
 		return (
 			<div>
@@ -27,12 +39,19 @@ class Message extends Component {
 						this.state.message.map((messageObj) =>{
 							return (
 								<li key={messageObj.id}>
-									<Link replace={true} to={`/home/message/detail/${messageObj.id}`}>{messageObj.title}</Link>
+									<Link to={`/home/message/detail/${messageObj.id}`}>{messageObj.title}</Link>
+									{/*
+										没有使用Link利用两个按钮实现了页面的跳转
+									*/}
+									<button onClick={() => {this.pushShow(messageObj.id)}}>push查看</button>&nbsp;&nbsp;
+									<button onClick={() => {this.replaceShow(messageObj.id)}}>replace查看</button>&nbsp;&nbsp;
 								</li>
 							)
 						})
 					}
 				</ul>
+				<button onClick={this.forword}>前进</button>
+				<button onClick={this.back}>后退</button>
 				<hr/>
 				<Route path="/home/message/detail/:id" component={Detail}/>
 			</div>
